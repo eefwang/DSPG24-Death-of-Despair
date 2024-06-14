81aec417224129cf640bc274b54596b6dab5730b
Yifan Wang <yifanwang@MacBook-Pro.local>
2024-06-13 23:33:55 -0400

    new

---
+++ Programs/.Master_Do_file.R
@@ -0,0 +1,29 @@
+#Master Do File
+library(here)
+
+##############
+#Data Cleaning 
+##############
+
+#Import Data
+rmarkdown::render(here("Programs", "00cl_Data_import.Rmd"))
+#Retrieve ACS data for 2018-2022
+
+rmarkdown::render(here("Programs", "00cl_Data_import.Rmd"))
+
+#Merge ACS, health rank, Death data, ruc codes for VA and Appalachian
+rmarkdown::render(here("Programs", "02cl_Merge_VA_APP_Data.Rmd"))
+
+
+###################
+#Data Visualization
+###################
+
+
+
+
+
+
+####################
+#Regression Analysis
+####################
