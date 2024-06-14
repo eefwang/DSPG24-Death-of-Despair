a5ad1ac43c1a2acfd0df5092390168a43edcb67a
Yifan Wang <yifanwang@MacBook-Pro.local>
2024-06-13 21:56:01 -0400

    add files

---
+++ Programs/Master_Do_file.R
@@ -0,0 +1,27 @@
+#Master Do File
+library(here)
+
+##############
+#Data Cleaning 
+##############
+
+#00Data Import
+rmarkdown::render(here("Programs", "00cl_Data_import.Rmd"))
+
+
+
+
+
+
+###################
+#Data Visualization
+###################
+
+
+
+
+
+
+####################
+#Regression Analysis
+####################
