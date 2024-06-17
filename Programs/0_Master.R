8be667cb9a897e6ae7976305997398ed234ab49a
BrynnaWert <brynnawert@vt.edu>
2024-06-17 13:24:19 -0400

    Hiya

--- Programs/0_Master.R
+++ Programs/0_Master.R
@@ -6,19 +6,19 @@ library(here)
 ##############
 
 #Import Data
-rmarkdown::render(here("Programs", "00cl_Data_import.Rmd"))
+rmarkdown::render(here("Programs", "1cl_01_Data_import.Rmd"))
 
 #Retrieve ACS data for 2018-2022
-rmarkdown::render(here("Programs", "01cl_ACS_Retrieval.Rmd"))
+rmarkdown::render(here("Programs", 1cl_02_ACS_Retrieval.Rmd"))
 
 #Merge ACS, health rank, Death data, ruc codes for VA and Appalachian
-rmarkdown::render(here("Programs", "02cl_Merge_VA_APP_Data.Rmd"))
+rmarkdown::render(here("Programs", "1cl_03_Merge_VA_APP_Data.Rmd"))
 
 #Merge Data with Map
-#rmarkdown::render(here("Programs", "03cl_Merged_Map_Data.Rmd"))
+#rmarkdown::render(here("Programs", "1cl_04_Merged_Map_Data.Rmd"))
 
 #Merge Data with Map
-rmarkdown::render(here("Programs", "04cl_Bartik_1822.Rmd"))
+rmarkdown::render(here("Programs", "1cl_05_Bartik_1822.Rmd"))
 
 ###################
 #Data Visualization
