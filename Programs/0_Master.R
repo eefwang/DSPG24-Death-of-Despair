5dab054b03cae49f22904f8b40cca090fc5d529e
Yifan Wang <yifanwang@MacBook-Pro.local>
2024-06-19 00:05:04 -0400

    new

--- Programs/0_Master.R
+++ Programs/0_Master.R
@@ -6,24 +6,23 @@ library(here)
 ##############
 
 #Import Data
-rmarkdown::render(here("Programs", "1cl_01_Data_import.Rmd"))
+rmarkdown::render(here("Programs/cleaning", "1cl_01_Data_import.Rmd"))
 
 #Retrieve ACS data for 2018-2022
-rmarkdown::render(here("Programs", "1cl_02_ACS_Retrieval.Rmd"))
+rmarkdown::render(here("Programs/cleaning", "1cl_02_ACS_Retrieval.Rmd"))
 
 #Merge ACS, health rank, Death data, ruc codes for VA and Appalachian
-rmarkdown::render(here("Programs", "1cl_03_Merge_VA_APP_Data.Rmd"))
+rmarkdown::render(here("Programs/cleaning", "1cl_03_Merge_VA_APP_Data.Rmd"))
 
 #Merge Data with Map
-#rmarkdown::render(here("Programs", "1cl_04_Merged_Map_Data.Rmd"))
+#rmarkdown::render(here("Programs/cleaning", "1cl_04_Merged_Map_Data.Rmd"))
 
 #Merge Data with Map
-rmarkdown::render(here("Programs", "1cl_05_Bartik_1822.Rmd"))
+rmarkdown::render(here("Programs/cleaning", "1cl_05_Bartik_1822.Rmd"))
 
 ###################
 #Data Visualization
 ###################
-
 
 
 
