993c24bbb900688b026204871e1df751e05ca932
Yifan Wang <yifanwang@MacBook-Pro.local>
2024-06-25 15:12:56 -0400

    NEW

--- Programs/line/2dv_09_line_va_DODbyYear_1822.R
+++ Programs/line/2dv_09_line_va_DODbyYear_1822.R
@@ -1,17 +1,15 @@
 # Load necessary libraries
 library(ggplot2)
 library(dplyr)
+library(here)
 
+#Data Import
+VA_DOD_1822 <- read.csv(here("Data/raw_data/MCOD VA 1822", "VA.DOD.1822.txt"), sep = "\t", header = TRUE)
 
-# Data inspection
-print(head(VA_DOD_1822))
-print(str(VA_DOD_1822))
-
-# Rename columns to remove spaces
-colnames(VA_DOD_1822) <- make.names(colnames(VA_DOD_1822))
-
-# Check the column names
-print(colnames(VA_DOD_1822))
+#Prepare data
+VA_DOD_1822 <- VA_DOD_1822 %>%
+  select(Year,Crude.Rate) %>%
+  na.omit()
 
 # Create the line plot
 ggplot(data = VA_DOD_1822, aes(x = Year, y = Crude.Rate)) +
